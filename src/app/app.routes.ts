02346c03ab5b5eeaa2b4fe5fccf72031fd9c52dd
SamuelSotomayor1 <andressotomayor477@gmail.com>
2025-06-19 22:45:12 -0400

    feat: register, login and navbar are updated, palette of color are defined

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
 import { Register } from './auth/register/register';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
     {path: 'login', component: Login},
     {path: 'register', component: Register},
+    {path: 'dashboard', component: Dashboard},
     { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gym-green': '#10B981',
+        'gym-green-light': '#34D399',
+        'gym-green-dark': '#059669',
+        'gym-mint': '#6EE7B7',
+        'gym-dark': '#1A1A1A',
+        'gym-gray': '#2D2D2D',
+        'gym-light': '#F0FDF4'
+      }
+    },
   },
   plugins: [],
 }

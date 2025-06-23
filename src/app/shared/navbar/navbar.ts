d14da58435600d0d303d4de01de65f638ffead6d
SamuelSotomayor1 <andressotomayor477@gmail.com>
2025-06-22 23:49:44 -0400

    feat: the handle of navbar is re-estructured with nav-item

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { NavItem } from '../nav-item/nav-item';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NavItem],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
-
-}
+  items = [
+    {
+      name: "Maquinas",
+      path: "/maquinas"
+    },
+    {
+      name: "Ejercicios",
+      path: "/ejercicios"
+    },
+    {
+      name: "Rutinas",
+      path: "/rutinas"
+    },
+    {
+      name: "Acerca de",
+      path:"/acerca"
+    }
+  ]
+};

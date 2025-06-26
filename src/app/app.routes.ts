7c44a4071ca14cdef6b6dac1ac25a6e74be4f62b
SamuelSotomayor1 <andressotomayor477@gmail.com>
2025-06-26 02:07:59 -0400

    feat: the about interface are updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/auth/login/login';
 import { Register } from './pages/auth/register/register';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { About } from './pages/about/about';
 
 export const routes: Routes = [
     {path: 'login', component: Login},
     {path: 'register', component: Register},
     {path: 'dashboard', component: Dashboard},
+    {path: 'about', component: About},
     { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
